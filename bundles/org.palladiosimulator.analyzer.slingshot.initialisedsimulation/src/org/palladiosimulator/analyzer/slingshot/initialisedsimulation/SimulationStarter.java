70759a9ad9155b0af274e3fdf461e0b40f6cc46e
Sarah Sophie Stieß <sarah.stiess@iste.uni-stuttgart.de>
2025-06-17 08:24:39 +0200

    remove unused launches, switch to relative URI in EObject (de)serialization.

--- bundles/org.palladiosimulator.analyzer.slingshot.initialisedsimulation/src/org/palladiosimulator/analyzer/slingshot/initialisedsimulation/SimulationStarter.java
+++ bundles/org.palladiosimulator.analyzer.slingshot.initialisedsimulation/src/org/palladiosimulator/analyzer/slingshot/initialisedsimulation/SimulationStarter.java
@@ -25,72 +25,101 @@ import de.uka.ipd.sdq.simucomframework.SimuComConfig;
 import de.uka.ipd.sdq.workflow.mdsd.blackboard.MDSDBlackboard;
 
 /**
- * Core Component of the Exploration.
+ * Initialises and runs a singles simulation and saves the results to file.
  *
- * Responsible for exploring new states.
- *
- * @author Sarah Stieß
+ * @author Sophie Stieß
  *
  */
 public class SimulationStarter {
 
 	private static final Logger LOGGER = Logger.getLogger(SimulationStarter.class.getName());
-	
-	private static final String resultFile = "state.json";
-	private static final String snapshotFile = "snapshot.json";
+
+	private static final String resultFile = "state.state";
+	private static final String snapshotFile = "snapshot.snapshot";
 
 	private final PCMResourceSetPartition initModels;
-	
+
 	final SimulationDriver driver;
 	final StateBuilder stateBuilder;
 
-	public SimulationStarter(final SimuComConfig config, final IProgressMonitor monitor, 
-			final MDSDBlackboard blackboard, final Path initStateLocation, final Path otherLocation, final Path resultLocation, final String nextStateId) {
+	/**
+	 * Create a new {@link SimulationStarter} and already initiate the simulation driver.
+	 * 
+	 * @param config
+	 * @param monitor
+	 * @param blackboard
+	 * @param initStateLocation
+	 * @param otherLocation
+	 * @param resultLocation
+	 */
+	public SimulationStarter(final SimuComConfig config, final IProgressMonitor monitor,
+			final MDSDBlackboard blackboard, final Path initStateLocation, final Path otherLocation,
+			final Path resultLocation, final String nextStateId) {
 		super();
-		this.initModels = (PCMResourceSetPartition) blackboard.getPartition(ConstantsContainer.DEFAULT_PCM_INSTANCE_PARTITION_ID);
-	
-		final InitState initstate = getInitState(initStateLocation);
-		final OtherInitThings otherInitThings = getOthers(otherLocation); // i'd prefer this to throw some error.... 
-		
-		// copy onyl after loading the snapshot, because of the model references. 
+		this.initModels = (PCMResourceSetPartition) blackboard
+				.getPartition(ConstantsContainer.DEFAULT_PCM_INSTANCE_PARTITION_ID);
+
+		// copy BEFORE loading the snapshot, because of the cost stereotype resolution
 		final URI resultFolder = URI.createFileURI(resultLocation.toString());
 		ArchitectureConfigurationUtil.copyToURI(initModels.getResourceSet(), resultFolder);
-		
-		final InitWrapper wrapper = new Preprocessor(this.initModels, initstate.getSnapshot(), otherInitThings.getIncomingPolicies()).createWrapper();
-		final SnapshotConfiguration snaphshotConfig = new SnapshotConfiguration(initstate.getPointInTime() > 0.0, otherInitThings.getSensibility(), config.getSimuTime());			
+
+		final InitState initstate = getInitState(initStateLocation);
+		final OtherInitThings otherInitThings = getOthers(otherLocation);
+
+		final InitWrapper wrapper = new Preprocessor(this.initModels, initstate.getSnapshot(),
+				otherInitThings.getIncomingPolicies()).createWrapper();
+		final SnapshotConfiguration snaphshotConfig = new SnapshotConfiguration(initstate.getPointInTime() > 0.0,
+				otherInitThings.getSensibility(), config.getSimuTime());
 		this.stateBuilder  = new StateBuilder(initstate.getId(), initstate.getPointInTime(), this.initModels, nextStateId);
-		
 		AdditionalConfigurationModule.snapConfigProvider.set(snaphshotConfig);
 		AdditionalConfigurationModule.eventsToInitOnProvider.set(wrapper);
 		AdditionalConfigurationModule.defaultStateProvider.set(stateBuilder);
-		
+
 		this.driver = Slingshot.getInstance().getSimulationDriver();
 		this.driver.init(config, monitor);
 	}
 
-	
+	/**
+	 * Load the initial state of the simulator from file.
+	 * 
+	 * Beware: Due to a bug(?) regarding the resolution and loading of the stereotypes,
+	 * this operation must only be called AFTER
+	 * {@link ArchitectureConfigurationUtil#copyToURI(org.eclipse.emf.ecore.resource.ResourceSet, URI)}. 
+	 * 
+	 * @param location location of the file to be loaded
+	 * @return state to initiate the simulator to
+	 */
 	private InitState getInitState(final Path location) {
 		final InitStateDeSerialization thing = new InitStateDeSerialization(this.initModels);
 		return thing.deserialize(location);
 	}
-	
+
+	/**
+	 * Load dditional configurations for initialising the simulator and the snapshot components from file. 
+	 * 
+	 * @param location location of the file to be loaded
+	 * @return additional configurations for initialising the simulator and the snapshot components.
+	 */
 	private OtherInitThings getOthers(final Path location) {
 		final OtherStuffDeserialization stuff = new OtherStuffDeserialization(this.initModels);
 		return stuff.deserialize(location);
 	}
 
-	public void simulateSingleState(final Path location) {
+	/**
+	 * Exectue the simulation and save the results to file.
+	 * 
+	 * @param resultLocation location to save the state and snapshot to.
+	 */
+	public void simulateSingleState(final Path resultLocation) {
 		LOGGER.info("********** DefaultGraphExplorer.explore() **********");
 
 		driver.start();
 		UpdateSPDUtil.reduceTriggerTime(PCMResourcePartitionHelper.getSPD(initModels), stateBuilder.getDuration());
-		
-	
-		final Path snapPath = Path.of(location.toString(), snapshotFile);
-		(new InitStateDeSerialization(this.initModels)).serialize(stateBuilder.buildInitState(), snapPath);
-		
 
-		final Path resultPath = Path.of(location.toString(), resultFile);
+		final Path snapPath = Path.of(resultLocation.toString(), snapshotFile);
+		(new InitStateDeSerialization(this.initModels)).serialize(stateBuilder.buildInitState(), snapPath);
+
+		final Path resultPath = Path.of(resultLocation.toString(), resultFile);
 		(new ResultStateSerialization()).serialize(stateBuilder.buildResultState(), resultPath);
 	}
 }
--- bundles/org.palladiosimulator.analyzer.slingshot.snapshot.serialization/src/org/palladiosimulator/analyzer/slingshot/snapshot/serialization/adapters/EObjectTypeAdapter.java
+++ bundles/org.palladiosimulator.analyzer.slingshot.snapshot.serialization/src/org/palladiosimulator/analyzer/slingshot/snapshot/serialization/adapters/EObjectTypeAdapter.java
@@ -1,9 +1,13 @@
 package org.palladiosimulator.analyzer.slingshot.snapshot.serialization.adapters;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.palladiosimulator.commons.emfutils.EMFLoadHelper;
@@ -16,20 +20,48 @@ public class EObjectTypeAdapter extends TypeAdapter<EObject> {
 
 	private final ResourceSet set;
 	
+	private final Map<String, URI> URIMap;
+	
 	public EObjectTypeAdapter(final ResourceSet set) {
 		super();
 		this.set = set;
+
+		URIMap = new HashMap<>();
+		for (final Resource resource : set.getResources()) {
+			
+			final URI uri = resource.getURI();
+			final String filename = uri.lastSegment();
+			
+			URIMap.put(filename, uri);
+		}
 	}
 
 	@Override
-	public void write(final JsonWriter out, final EObject value) throws IOException {
+	public void write(final JsonWriter out, final EObject value) throws IOException {		
 		final URI uri = EcoreUtil.getURI(value);
-		out.value(uri.toString());
+		if (uri.isFile()) {
+			final String file = new File(uri.toFileString()).toPath().getFileName().toString();
+			final String fragment = uri.fragment();
+
+			final URI relativeUri = URI.createURI(file).appendFragment(fragment); 
+			out.value(relativeUri.toString());
+		} else {
+			out.value(uri.toString());
+		}
+					
 	}
 
 	@Override
 	public EObject read(final JsonReader in) throws IOException {
 		final String s = in.nextString();
-		return EMFLoadHelper.loadAndResolveEObject(set, s);
+		
+		final String file = s.split("#")[0];
+		
+		if (URIMap.containsKey(file)) {
+			final URI uri = URIMap.get(file).appendFragment(s.split("#")[1]);
+			return EMFLoadHelper.loadAndResolveEObject(set, uri); 
+		}
+				
+		return EMFLoadHelper.loadAndResolveEObject(set, s); 
 	}
 }
